ca453e8b98c2b110142f24d0ce2efd109d1b9d1f
Jon Rhymes <jonrhymes@Alisons-MacBook-Air.local>
2020-10-01 11:27:39 -0400

    changed var to temp

--- js/buttonsGrid.js
+++ js/buttonsGrid.js
@@ -3,7 +3,7 @@ const button5 = document.getElementById("btn5")
 
 
 function rotate() {
-    let variableNum = btn1.innerHTML;
+    let temp = btn1.innerHTML;
     btn1.innerHTML = btn4.innerHTML;
     btn4.innerHTML = btn7.innerHTML;
     btn7.innerHTML = btn8.innerHTML;
@@ -11,7 +11,7 @@ function rotate() {
     btn9.innerHTML = btn6.innerHTML;
     btn6.innerHTML = btn3.innerHTML;
     btn3.innerHTML = btn2.innerHTML;
-    btn2.innerHTML = variableNum;
+    btn2.innerHTML = temp;
 }
 
 button5.addEventListener('click', rotate)
