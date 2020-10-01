368268ba9a2223ac40142e430daeefee9a8920b1
Jon Rhymes <jonrhymes@Alisons-MacBook-Air.local>
2020-10-01 11:21:14 -0400

    hackerrank challenge

---
+++ js/buttonsGrid.js
@@ -0,0 +1,17 @@
+
+const button5 = document.getElementById("btn5")
+
+
+function rotate() {
+    let variableNum = btn1.innerHTML;
+    btn1.innerHTML = btn4.innerHTML;
+    btn4.innerHTML = btn7.innerHTML;
+    btn7.innerHTML = btn8.innerHTML;
+    btn8.innerHTML = btn9.innerHTML;
+    btn9.innerHTML = btn6.innerHTML;
+    btn6.innerHTML = btn3.innerHTML;
+    btn3.innerHTML = btn2.innerHTML;
+    btn2.innerHTML = variableNum;
+}
+
+button5.addEventListener('click', rotate)
